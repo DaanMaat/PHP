584dfc5e81f2888507b4c29ac63bd5d963f11bb5
Daan Maat <dmaat@roc-dev.com>
2022-04-26 14:15:53 +0200

    Opdrachten H02

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+
+$a = "Ik heb niet gefaald";
+$b = "die niet werken";
+$c = 10000;
+echo $a.". "."Ik heb alleen ".$c." manieren gevonden "."$b";
+
+$d = "die geen fout";
+$e = "en";
+$f = "niet";
+echo "M".$e."sen ".$d.$e." maken werken ".$f;
+
+$g = "ry";
+$h = ", but";
+$i = "fail";
+echo "T".$g." and ".$i.$h." never ".$i." to t".$g;
