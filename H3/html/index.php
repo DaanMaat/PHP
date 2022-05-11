31d5557bdec316cd12eef3bf850e05dd26d3fd18
Daan Maat <dmaat@roc-dev.com>
2022-05-11 10:29:26 +0200

    Website code update

--- H3/html/index.php
+++ H3/html/index.php
@@ -8,44 +8,64 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Katten</title>
+    <title>Niet werkende website</title>
     <style>
+        *{
+            padding: 0;
+            margin: 0;
+            box-sizing: border-box;
+        }
     body{
         text-align: center;
     }
     .rood{
-        border: red, solid, 6px;
+        border: 6px solid red;
+        width: 100px;
+        height: 100px;
     }
     .groen{
-        border: green, solid, 6px;
+        border: 6px solid green;
+        width: 100px;
+        height: 100px;   
+    }
+    .Icon{
+        width: 50px;
+        height: 50px;
+    }
+    .Cats{
+        width: 100px;
+        height: 100px;
     }
     </style>
 </head>
 <body>
     <?php
-        for($i = 0; i<=10; $i++){
-            echo "<img src ='kat".$i.".jpg>";
-        }
-
-        $Katten = array("kat1.jpg", "kat2.jpg", "kat3.jpg", "kat4.jpg", "kat5.jpg", "kat6.jpg", "kat7.jpg", "kat8.jpg", "kat9.jpg", "kat10.jpg");
-        foreach($Katten as $Kat){
-            echo $Kat;
+        for($i = 1; $i<=10; $i++){
+            echo "<img class='Cats' src='kat".$i.".jpg'> ";
         }
         echo "<br>";
-        for($j = 0; j<21; j++){
-            for($k; $k<$j; $k++){
+        $Katten = array("kat1.jpg", "kat2.jpg", "kat3.jpg", "kat4.jpg", "kat5.jpg", "kat6.jpg", "kat7.jpg", "kat8.jpg", "kat9.jpg", "kat10.jpg");
+        foreach($Katten as $Kat){
+            echo "<img class='Cats' src='".$Kat."'>";
+        }
+        echo "<br> <br>";
+        for($j = 0;$j<23;$j++){
+            for($k = 0;$k<$j;$k++){
                 echo "*";
             }
             echo "*<br>";
-        }
+        } 
+        echo "<br>"; 
         for($x = 35;$x>=25;$x--){
             echo "hoppelepee";
         }
-        for($i = 0; i<=10; $i++){
-            if($i % 2 == 0){
-            echo "<img class="rood" src ='kat".$i.".jpg>";
-        } else {echo "<img class="groen" src ='kat".$i.".jpg>";}
+        echo '<br> <br>';
+        for($l = 1;$l <= 10; $l++){
+            if($l % 2 == 0){
+            echo "<img class='rood' src ='kat".$l.".jpg'>";
+        } else {echo "<img class='groen' src ='kat".$l.".jpg'>";}
         }
+        echo "<br> <br>";
         $leeftijd = 65;
         $prijs;
         if($leeftijd <= 65 && $leeftijd > 12){
@@ -53,46 +73,70 @@
         } else if ($leeftijd <= 3){
             $prijs = 0;
         } else {$prijs = 5;};
-        echo $prijs;
+        echo $prijs."<br> <br>";
         
         $spartelkuikens = 25;
         $Waterbuffels = 32;
         $Plonsmderin = 11;
         $Bommetje = 23;
-        for($spartelkuikens = 25; $spartelkuikens >= 5; $spartelkuikens-5){
-            echo "<img src='Swimmer.png'>";
+        while($spartelkuikens >= 5){
+            echo "<img class='Icon' src='Swimmer.png'>";
+            $spartelkuikens-= 5;
         }
-        for($Waterbuffels = 32; $Waterbuffels >= 5; $Waterbuffels-5){
-            echo "<img src='Swimmer.png'>";
+        echo "<br>";
+        while($Waterbuffels >= 5){
+            echo "<img class='Icon' src='Swimmer.png'>";
+            $Waterbuffels -= 5;
         }
-        for($Plonsmderin = 11; $Plonsmderin >= 5; $Plonsmderin-5){
-            echo "<img src='Swimmer.png'>";
+        echo "<br>";
+        while($Plonsmderin > 5){
+            echo "<img class='Icon' src='Swimmer.png'>";
+            $Plonsmderin -= 5;
         }
-        for($Bommetje = 23; $Bommetje >= 5; $Bommetje-5){
-            echo "<img src='Swimmer.png'>";
-            $Bommetje - 5;
+        echo "<br>";
+        while($Bommetje > 5){
+            echo "<img class='Icon' src='Swimmer.png'>";
+            $Bommetje -= 5;
         }
+        echo "<br> <br>";
         $Zwemclubs["Spartelkuikens"] = 25;
         $Zwemclubs["Waterbuffels"] = 32;
         $Zwemclubs["Plonsmderin"] = 11;
         $Zwemclubs["Bommetje"] = 23;
-        foreach($Zwemclubs as $label => waarde){
+        foreach($Zwemclubs as $waarde){
            $Swimmers = floor($waarde / 5);
-           for($Swimmers > 0; $Swimmers--){
-               echo "<img src='Swimmer.png'>"
+           for($Swimmers > 0; $Swimmers--;){
+               echo "<img class='Icon' src='Swimmer.png'>";
            }
-           echo "<br>"
+           echo "<br>";
         }
-        $Kappersagenda[9.15] = "Mevr. Pietersen";
-        $Kappersagenda[9.30] = "Mevr. Willems";
-        $Kappersagenda[9.45] = "";
-        $Kappersagenda[10.00] = "Paul van den Broek";
-        $Kappersagenda[10.15] = "Karel de Meeuw";
-        $Kappersagenda[10.30] = "";
+        echo "<br>";
+        $Kappersagenda = array("Mevr. Pietersen", "Mevr. Willems", "", "Paul van den Broek", "Karel de Meeuw", "");
         print ("De volgende momenten zijn nog beschikbaar:<ul>");
-        foreach($Kappersagenda as $tijd => $afspraak);
-        if($afspraak == ""){
+        foreach($Kappersagenda as $tijd => $afspraak){
+            if($afspraak == ""){
+            switch ($tijd) {
+                case 0:
+                $tijd = "9:15";
+                break;
+                case 1:
+                $tijd = "9:30";
+                break;
+                case 2:
+                $tijd = "9:45";
+                break;
+                case 3: 
+                $tijd = "10:00";
+                break;
+                case 4:
+                $tijd = "10:15";
+                break;
+                case 5:
+                $tijd = "10:30";
+                break;
+            }
             print("<li>".$tijd."</li>");
+            }
         }
     ?>
 </body>
